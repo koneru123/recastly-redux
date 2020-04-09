53fc22968296c1e345c9b81e9f4b968b624a5130
Brian de Regt <brian.deregt@gmail.com>
2020-04-09 01:03:19 -0400

    cleaned up videoList (reducer), currentVideo reducer functioning

--- src/reducers/videoList.js
+++ src/reducers/videoList.js
@@ -1,18 +1,10 @@
 import Redux from 'redux';
 
 var videoListReducer = (state = [], action) => {
-  //TODO: define a reducer for the videoList field of our state.
-  // console.log('state');
-  // console.log(state);
-  // console.log('action');
-  // console.log(action);
     if (action.type === 'CHANGE_VIDEO_LIST') {
-      console.log([action.videos[0]]);
       return action.videos.map(video => {
         return video;
       });
-
-      //return state = [action.videos[0]];
     } else {
       return state;
     }
